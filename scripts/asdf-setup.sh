fe3168f19ac46883039d5b9884a51472aeead0cf
Chris Taylor <chris11.taylor@nttdata.com>
2024-05-15 10:27:26 -0500

    Update paths for launch user

--- scripts/asdf-setup.sh
+++ scripts/asdf-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-git clone https://github.com/asdf-vm/asdf.git "/root/.asdf" --branch v0.11.3
-source "/root/.asdf/asdf.sh"
+git clone https://github.com/asdf-vm/asdf.git "/home/launch/.asdf" --branch v0.11.3
+source "/home/launch/.asdf/asdf.sh"
 
